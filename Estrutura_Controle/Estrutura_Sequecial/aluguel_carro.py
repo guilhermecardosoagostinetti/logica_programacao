9935450278a2752085573a6386c29d9aeea10084
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-05-30 16:18:54 -0300

    Update aluguel_carro.py

---
+++ Estrutura_Controle/Estrutura_Sequecial/aluguel_carro.py
@@ -0,0 +1,13 @@
+## CUSTO DO ALUGUEL DE CARRO
+## AUTOR: PROF. GUILHERME CARDOSO AGOSTINETTI
+
+# ENTRADA DE DADOS
+km = float(input("kilometragem Percorrida: "))
+dias = float(input("Dias Usado: "))
+
+
+# PROCESSAMENTO 
+valor_total = 0.20*km+90*dias
+
+# SAÍDA DE DADOS
+print("O Valor total é: ", valor_total)
