6be3e2f2f6c75564becab22d952dbb36601a4ced
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-05-30 16:05:39 -0300

    Create converter_dolar.py

---
+++ Estrutura_Controle/Estrutura_Sequecial/converter_dolar.py
@@ -0,0 +1,12 @@
+## CONVERTER REAL EM DÓLAR
+## AUTOR: PROF. GUILHERME CARDOSO AGOSTINETTI
+
+# ENTRADA DE DADOS
+dollar = 4.90
+real = float(input("Digite quanto você tem na Carteira em Reais: "))
+
+# PROCESSAMENTO 
+valor_dollar = real/dollar
+
+# SAÍDA DE DADOS
+print("VOCÊ TEM :", valor_dollar, "EM DOLLAR")
