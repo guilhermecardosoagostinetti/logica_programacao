051c92874889454f022480e57e4b4ac68f09a7e5
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-05-30 15:26:47 -0300

    Create operacao_dois_numeros.py

---
+++ Estrutura_Controle/Estrutura_Sequecial/operacao_dois_numeros.py
@@ -0,0 +1,19 @@
+## QUATRO OPERAÇÕES COM DOIS NÚMEROS
+## AUTOR: PROF. GUILHERME CARDOSO AGOSTINETTI
+
+# ENTRADA DE DADOS
+num1 = float(input("Número 1: "))
+num2 = float(input("Número 2: "))
+
+
+# PROCESSAMENTO 
+soma = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1/num2
+
+# SAÍDA DE DADOS
+print("SOMA:", soma)
+print("SUBTRAÇÃO:", sub)
+print("MULTIPLICAÇÃO:", mult)
+print("DIVISÃO:", div)
