dace8ad746d6811b30eebf38d227e2484542c251
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-05-30 14:54:24 -0300

    Create media_aritmetica_bimestral.py

---
+++ Estrutura_Controle/Estrutura_Sequecial/media_aritmetica_bimestral.py
@@ -0,0 +1,13 @@
+## MÉDIA ARITMÉTICA 4 BIMESTRES 
+
+# ENTRADA DE DADOS
+N1 = float(input("N1: "))
+N2 = float(input("N2: "))
+N3 = float(input("N3: "))
+N4 = float(input("N4: "))
+
+# PROCESSAMENTO 
+MA = (N1+N2+N3+N4)/4
+
+# SAÍDA DE DADOS
+print(MA)
