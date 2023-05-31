98c04692b850c883cd227faa76eec5448c07c4ac
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-05-31 16:16:46 -0300

    Create declaracao_variaiveis.py

---
+++ Tipos_Primitivos_Dados/declaracao_variaiveis.py
@@ -0,0 +1,8 @@
+## DECLARAÇAO DE VARIÁVEIS
+# Autor: Prof. Guilherme Cardoso Agostinetti
+
+#Entrada de Dados
+NB = float(input("NOTA:"))
+NA = str(input("NOME:"))
+NMat = int(input("MATRÍCULA: "))
+SX = bool(input("SEXO(True = Menino, False = Menina): "))
