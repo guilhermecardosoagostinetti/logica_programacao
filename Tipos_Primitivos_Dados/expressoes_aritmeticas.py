06bdf2c4ccf58f901380e764f91bb60569864ba4
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-05-31 16:32:52 -0300

    Create expressoes_aritmeticas.py

---
+++ Tipos_Primitivos_Dados/expressoes_aritmeticas.py
@@ -0,0 +1,25 @@
+## DECLARAÇAO DE VARIÁVEIS
+# Autor: Prof. Guilherme Cardoso Agostinetti
+
+A = 5
+B = 10
+C = -8
+D = 1.5
+
+a= 2*A%3-C
+print(a)
+
+b= (-2*C)**0.5 // 4
+print(b)
+
+c= ((20 // 3) // 3) + 8**2/2
+print(c)
+
+d = (30%4*3**3)*(-1)
+print(d)
+
+e = (-C)**2+(D*10)/A
+print(e)
+
+f = ((A)**(B/A))**0.5+C*D
+print(f)
