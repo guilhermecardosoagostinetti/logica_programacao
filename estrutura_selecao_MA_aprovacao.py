e5e9ce8d050fdbdbc1ecd577c6aa72f8f63c51d5
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-05-31 16:09:46 -0300

    Create estrutura_selecao_MA_aprovacao.py

---
+++ estrutura_selecao_MA_aprovacao.py
@@ -0,0 +1,16 @@
+## MÉDIA ARITMÉTICA 4 BIMESTRES COM APROVAÇÃO
+
+# ENTRADA DE DADOS
+N1 = float(input("N1: "))
+N2 = float(input("N2: "))
+N3 = float(input("N3: "))
+N4 = float(input("N4: "))
+
+# PROCESSAMENTO 
+MA = (N1+N2+N3+N4)/4
+
+# SAÍDA DE DADOS
+if (MA>=7):
+    print("ALUNO APROVADO")
+
+print(MA)
