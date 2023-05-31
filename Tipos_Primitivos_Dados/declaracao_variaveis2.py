d86381abae5846333ce07c1098bc9ee1b46cfa1c
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-05-31 16:24:57 -0300

    Create declaracao_variaveis2.py

---
+++ Tipos_Primitivos_Dados/declaracao_variaveis2.py
@@ -0,0 +1,12 @@
+
+placa = "Pare!"
+furos = 2
+degraus = 5
+maca = True
+horas = 3.5
+sexo = False
+
+
+print("A placa ",placa, "tinha", furos, "furos de bala" )
+print("Josefina Subiu",degraus,"Degraus para pegar a maça", maca)
+print("Alberta levou", horas, "horas para chegar ao hospital onde concebeu uma", sexo) 
