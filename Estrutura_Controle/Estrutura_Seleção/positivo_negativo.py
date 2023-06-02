3f9b588b6df4563199f6702447453333bb40f4e9
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-06-02 15:26:49 -0300

    Create positivo_negativo.py

---
+++ Estrutura_Controle/Estrutura_Seleção/positivo_negativo.py
@@ -0,0 +1,10 @@
+## VERIFICAR SE ANO É BISSEXTO 
+# Autor: Guilherme Cardoso Agostinetti
+
+numero = float(input("NÚMERO: "))
+
+if(numero > 0): 
+    print("É postivo")
+if(numero < 0):
+    print("É negativo")
+
