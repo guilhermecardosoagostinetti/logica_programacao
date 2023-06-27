cf9cde8f05d0b0c8253127d2907d8e5dc08753c3
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-06-27 18:06:27 -0300

    1

---
+++ Estrutura_Controle/Estrutura_Seleção/gastos.py
@@ -0,0 +1,8 @@
+
+salario = float(input("SALÁRIO:"))
+gatos = float(input("GASTOS:"))
+
+if salario>= gastos:
+    print("VOCÊ ESTA DENTRO DO ORÇAMENTO")
+else:
+    print("VOCÊ ESTA FORA DO ORÇAMENTO")
