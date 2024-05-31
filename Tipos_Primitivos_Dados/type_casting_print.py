9701029a141db3551d5542428bbe6e3a2916cdc1
guilherme.agostinetti <79881424+GuilhermeCardosoAgostinetti@users.noreply.github.com>
2024-05-31 12:03:17 -0300

    Update type_casting_print.py

--- Tipos_Primitivos_Dados/type_casting_print.py
+++ Tipos_Primitivos_Dados/type_casting_print.py
@@ -12,7 +12,8 @@ print(type(maior_idade))
 
 # Casting
 
-# Pelo python ser dinamicamente tipado, o proproi python convertes seus dados pelo interpretador.
+# Pelo python ser dinamicamente tipado, o proproi python convertes 
+# seus dados pelo interpretador.
 
 # ------------------ Para Int---------------------------
 x = 2
@@ -20,7 +21,9 @@ y = 2.8
 z = '2'
 
 print(x, y, z)
-print(type(x), type(y), type(z))
+print("A variável x é do tip:", type(x))
+print("A variável y é do tip:", type(y))
+print("A variável z é do tip:", type(z))
 
 # Convertendo com o casting para int
 
@@ -29,8 +32,9 @@ y = int(y)
 z = int(z)
 
 print(x, y, z)
-print(type(x), type(y), type(z))
-
+print("A variável x é do tip:", type(x))
+print("A variável y é do tip:", type(y))
+print("A variável z é do tip:", type(z))
 # perceba que agora só temos variáveis inteiras
 
 # ------------------ Para float---------------------------
@@ -40,12 +44,35 @@ b = 2
 c = '2.3'
 
 print(a, b, c)
-print(type(a), type(b), type(c))
+print("A variável a é do tip:", type(a))
+print("A variável b é do tip:", type(b))
+print("A variável c é do tip:", type(c))
 
 a = float(a)
 b = float(b)
 c = float(c)
 
 print(a, b, c)
-print(type(a), type(b), type(c))
+print("A variável a é do tip:", type(a))
+print("A variável b é do tip:", type(b))
+print("A variável c é do tip:", type(c))
 
+# ------------------ Para String---------------------------
+
+t = 'oi'
+s = 1
+r = 2.4
+
+print(t, s, r)
+print("A variável t é do tip:", type(t))
+print("A variável s é do tip:", type(s))
+print("A variável r é do tip:", type(r))
+
+t = str(t)
+s = str(s)
+r = str(r)
+
+print(t, s, r)
+print("A variável t é do tip:", type(t))
+print("A variável s é do tip:", type(s))
+print("A variável r é do tip:", type(r))
