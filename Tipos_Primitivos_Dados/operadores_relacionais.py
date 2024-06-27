57b9f9eea6537a000de16555af1a2f4db0d1f59a
guilherme.agostinetti <79881424+GuilhermeCardosoAgostinetti@users.noreply.github.com>
2024-06-27 19:38:58 -0300

    Create operadores_relacionais.py

---
+++ Tipos_Primitivos_Dados/operadores_relacionais.py
@@ -0,0 +1,19 @@
+# Operadores Relacionais
+
+# Declaração de Variáveis
+a = 5
+b = 4
+
+# Verificando as operações Relacionais
+
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+
+
+# Expressões Lógicas
+
+print(4 < 6 and 7 == 5)
+print(6 < 2 or 15/3 == 5)
+
